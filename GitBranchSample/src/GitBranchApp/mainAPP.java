fa5100572dbc5cd9137274ad256d950984d914a6
Teddy <whatever016015@gmail.com>
2021-06-08 16:09:22 +0800

    Add Math Branch

--- GitBranchSample/src/GitBranchApp/mainAPP.java
+++ GitBranchSample/src/GitBranchApp/mainAPP.java
@@ -4,5 +4,13 @@ public class mainAPP {
 	public static void main(String[] args) {
 		// 組長第一版本
 		System.out.println("1st Version");
+		Math Math = new Math();
+		System.out.println();
+		System.out.println(Math.add(10, 20));
+	}
+}
+class Math{
+	int add(int a, int b) {
+		return a + b;
 	}
 }
